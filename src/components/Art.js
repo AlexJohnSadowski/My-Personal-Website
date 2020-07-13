6c1b143c8b284d0fad86caf50e2fd64f43dd267f
Alex <sadovsky.aleksander@gmail.com>
2020-07-13 17:20:32 +0200

    image overhaul

--- src/components/Art.js
+++ src/components/Art.js
@@ -10,10 +10,10 @@ export default function Art() {
             <div className="title-section__art">
                 <div className="title-sub__art" data-aos="fade-right">Perhaps would you like to see something <span className="bw" data-aos="flip-up">non-IT related?</span></div>
             </div>
-            <div className="container card card-art-one" data-aos="fade-up-right" data-aos-delay='1000'/>
-            <div className="container card card-art-two" data-aos="flip-up" data-aos-delay='1000'/>
-            <div className="container card card-art-three" data-aos="fade-left" data-aos-delay='400'/>
-            <div className="container card card-art-four" id='blog' data-aos="fade-up-right" data-aos-delay='400'/>
+            <div className="container_blog card card-art-one" data-aos="fade-up-right" data-aos-delay='1000'/>
+            <div className="container_blog card card-art-two" data-aos="flip-up" data-aos-delay='1000'/>
+            <div className="container_blog card card-art-three" data-aos="fade-left" data-aos-delay='400'/>
+            <div className="container_blog card card-art-four" id='blog' data-aos="fade-up-right" data-aos-delay='400'/>
             <div className="title-section__art--two" data-aos="flip-up" data-aos-delay='400'>
                 <div className="title-sub__art--two" >Or maybe do you like <span className="bw">to read?</span></div>
             </div>
